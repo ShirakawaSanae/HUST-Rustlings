b3939e25964432fa2b5b18a33a4ed96313fc29e5
ShirakawaSanae <sunj1ngwen@mail.ustc.edu.cn>
2025-01-24 21:58:47 +0800

    update

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -2,11 +2,14 @@
 //
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a
 // hint.
-
-// I AM NOT DONE
+//use std::io;
 
 fn main() {
-    let x;
+     let x = 11;
+    // let x: f32 = 10.0; 
+    //let mut input = String::new();
+    //io::stdin().read_line(&mut input).expect("input error");
+    //let x: u32 = input.trim().parse().expect("not an integer");
     if x == 10 {
         println!("x is ten!");
     } else {
